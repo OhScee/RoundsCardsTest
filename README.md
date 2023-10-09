b7b50abd64d7e3dcef4c51efcbbe2c6822971d21
ohscee <ohscee@gmail.com>
2023-10-09 19:08:54 -0400

    feat: project setup

---
+++ README.md
@@ -0,0 +1,1 @@
+First test at card making
---
+++ TestCard.cs
@@ -0,0 +1,41 @@
+﻿using BepInEx;
+using UnboundLib;
+using UnboundLib.Cards;
+using HarmonyLib;
+using CardChoiceSpawnUniqueCardPatch.CustomCategories;
+using SimpleCards.Cards;
+
+namespace SimpleCards
+{
+    // These are the mods required for our mod to work
+    [BepInDependency("com.willuwontu.rounds.BlockForcePatch", BepInDependency.DependencyFlags.HardDependency)]
+    [BepInDependency("com.willis.rounds.unbound", BepInDependency.DependencyFlags.HardDependency)]
+    [BepInDependency("pykess.rounds.plugins.moddingutils", BepInDependency.DependencyFlags.HardDependency)]
+    [BepInDependency("pykess.rounds.plugins.cardchoicespawnuniquecardpatch", BepInDependency.DependencyFlags.HardDependency)]
+    // Declares our mod to Bepin
+    [BepInPlugin(ModId, ModName, Version)]
+    // The game our mod is associated with
+    [BepInProcess("Rounds.exe")]
+    public class CardTest : BaseUnityPlugin
+    {
+        private const string ModId = "com.vscee.rounds.SimpleCards";
+        private const string ModName = "SimpleCards";
+        public const string Version = "0.0.1"; // What version are we on (major.minor.patch)?
+        public const string ModInitials = "SC";
+        
+        public static CardTest instance { get; private set; }
+
+
+        void Awake()
+        {
+            // Use this to call any harmony patch files your mod may have
+            var harmony = new Harmony(ModId);
+            harmony.PatchAll();
+        }
+        void Start()
+        {
+            instance = this;
+            CustomCard.BuildCard<EvasiveManeuvers>(); 
+        }
+    }
+}
